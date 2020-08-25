3c07903c7c015f4f61b065770db0b6ff1f2bc649
Bruno Vieira <brunorick11@gmail.com>
2020-08-24 23:45:38 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Gerador de Certificado</title>
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<link rel="stylesheet" href="css/style.css">
+<!------ Include the above in your HEAD tag ---------->
+</head>
+<body>
+<div class="container">
+
+    <div class="row" id="pwd-container">
+        <div class="col-md-4"></div>
+
+        <div class="col-md-4">
+            <section class="login-form">
+                <form method="post" action="userCertificate.php" role="login">
+                    <h3 class="text-center">Gerador de Certificado</h3>
+                    <input type="name" name="name" placeholder="Nome" required class="form-control input-lg"/>
+
+                    <input type="lastname" name="lastname" class="form-control input-lg" id="lastname" placeholder="Sobrenome" required="" />
+
+
+                    <div class="pwstrength_viewport_progress"></div>
+
+
+                    <button type="submit" name="go" class="btn btn-lg btn-primary btn-block">Gerar</button>
+
+                </form>
+            </section>
+        </div>
+
+        <div class="col-md-4"></div>
+
+
+    </div>
+
+</div>
+
+</body>
+</html>
---
+++ userCertificate.php
@@ -0,0 +1,25 @@
+<?php
+
+$image = imagecreatefromjpeg('certificado.jpg');
+
+$titleColor = imagecolorallocate($image,0,0,0);
+$grey = imagecolorallocate($image, 100,100,100);
+$font = 'Bevan-Regular.ttf';
+$font_path1 = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'fonts'. DIRECTORY_SEPARATOR . 'Bevan' . DIRECTORY_SEPARATOR .'Bevan-Regular.ttf';
+$font_path2 = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'fonts'. DIRECTORY_SEPARATOR . 'Playball' . DIRECTORY_SEPARATOR .'Playball-Regular.ttf';
+
+$name = $_POST['name'];
+$lastname = $_POST['lastname'];
+$fullname = $_POST['name'] . " " . $_POST['lastname'];
+
+imagettftext($image, 32,0, 314,150, $titleColor,$font_path1, utf8_decode("CERTIFICADO"));
+imagettftext($image, 20,0, 214,250, $titleColor,$font_path1, utf8_decode("Certificamos que o:"));
+imagettftext($image, 20,0, 214,380, $titleColor,$font_path1, utf8_decode("É o mais brabo de todos, e quem discordar"));
+imagettftext($image, 20,0, 214,410, $titleColor,$font_path1, utf8_decode("é um grandessíssimo otário."));
+imagettftext($image, 32,0, 350,320, $titleColor,$font_path2, utf8_decode($fullname));
+imagestring($image, 5, 580 ,600,utf8_decode("Concluído em: ") . date("d/m/Y"), $titleColor);
+
+header('Content-Type: image/jpeg');
+
+imagejpeg($image);
+imagedestroy($image);
